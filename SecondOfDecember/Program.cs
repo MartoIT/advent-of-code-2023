03bf10c39042acdfa6a8c5a09060d76ef04942cc
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-12-21 12:11:24 +0200

    Wokring on a logic in nested loops

--- SecondOfDecember/Program.cs
+++ SecondOfDecember/Program.cs
@@ -1,32 +1,71 @@
 ﻿
-string input = File.ReadAllText("D:\\advent-of-code-2023\\SecondOfDecember\\input.txt");
+//string input = File.ReadAllText("D:\\advent-of-code-2023\\SecondOfDecember\\input.txt");
+string input = File.ReadAllText("C:\\Мартин\\Moi\\advent of code\\SecondOfDecember\\SecondOfDecember\\input.txt");
 
 string[] inputToArray = input.Split("\n");
-int redInHand = 0;
-int blueInHand = 0;
-int greenInHand = 0;
+
+
+int redPossibleGame = 12;
+int bluePossibleGame = 14;
+int greenPossibleGame = 13;
+
+int idOfThePossibleGames = 0;
+
+int chek = 0;
 
 for (int i = 0; i < inputToArray.Length; i++)
 {
+    int redInHand = 0;
+    int blueInHand = 0;
+    int greenInHand = 0;
     string current = inputToArray[i];
     string[] newLine = current.Split(": ");
 
     for (int j = 1; j < newLine.Length; j += 2)
     {
+        
         string currentLine = newLine[j];
-
-        for (int k = 0; k < currentLine.Length; k++)
+        string[] currentLine2 = currentLine.Split("; ");
+        
+        for (int k = 0; k < currentLine2.Length; k++)
         {
+           
+            string[] chekHend = currentLine2[k].Split("\n");
+            for (int l = 0; l < chekHend.Length; l++)
+            {
+               
+                string inside = chekHend[l];
+                string[] insideLine = inside.Split(", ");
+                for (int m = 0; m < insideLine.Length; m++)
+                {
+
+                    Console.WriteLine(insideLine[m]);
+
+                }
+
+                if (redInHand <= redPossibleGame && blueInHand <= bluePossibleGame && greenInHand <= greenPossibleGame)
+                {
+
+                    chek = 0;
+
+                }else
+                {
+                    chek += 1;
+                }
+                Console.WriteLine("New Line");
+
+            }
+
             string currentWord = "";
             char currentChar = currentLine[k];
 
-            if(k+1 < currentLine.Length) 
+            if (k + 1 < currentLine.Length)
             {
 
                 char nextChar = currentLine[k + 1];
                 if (Char.IsDigit(currentChar) && Char.IsDigit(nextChar))
                 {
-                    
+
                     int currentDigit = int.Parse($"{currentChar}{nextChar}");
                     string firstLetter = currentLine[k + 3].ToString();
                     string secondLetter = currentLine[k + 4].ToString();
@@ -36,8 +75,7 @@ for (int i = 0; i < inputToArray.Length; i++)
 
                     if (currentWord == "red")
                     {
-                        Console.WriteLine(currentDigit);
-                        Console.WriteLine(currentWord);
+                        redInHand += currentDigit;
                         k++;
                         continue;
                     }
@@ -48,8 +86,7 @@ for (int i = 0; i < inputToArray.Length; i++)
                         currentWord += $"{fourthLetter}";
                         if (currentWord == "blue")
                         {
-                            Console.WriteLine(currentDigit);
-                            Console.WriteLine(currentWord);
+                            blueInHand += currentDigit;
                             k++;
                             continue;
                         }
@@ -60,8 +97,7 @@ for (int i = 0; i < inputToArray.Length; i++)
                             currentWord += $"{fifthLetter}";
                             if (currentWord == "green")
                             {
-                                Console.WriteLine(currentDigit);
-                                Console.WriteLine(currentWord);
+                                greenInHand += currentDigit;
                                 k++;
                                 continue;
                             }
@@ -72,22 +108,21 @@ for (int i = 0; i < inputToArray.Length; i++)
 
                 }
             }
-            
 
-            
-           
+
+
+
             if (Char.IsDigit(currentChar))
             {
+                int currentDigit = int.Parse($"{currentChar}");
                 string firstLetter = currentLine[k + 2].ToString();
                 string secondLetter = currentLine[k + 3].ToString();
                 string thirdLetter = currentLine[k + 4].ToString();
 
                 currentWord += $"{firstLetter}{secondLetter}{thirdLetter}";
-                if(currentWord == "red")
+                if (currentWord == "red")
                 {
-                    //redInHand += int.Parse($"{currentChar}");
-                    Console.WriteLine(currentChar);
-                    Console.WriteLine(currentWord);
+                    redInHand += currentDigit;
                     continue;
                 }
                 if (k + 5 < currentLine.Length)
@@ -96,9 +131,7 @@ for (int i = 0; i < inputToArray.Length; i++)
                     currentWord += $"{fourthLetter}";
                     if (currentWord == "blue")
                     {
-                        Console.WriteLine(currentChar);
-                        Console.WriteLine(currentWord);
-                        //blueInHand += int.Parse($"{currentChar}");
+                        blueInHand += currentDigit;
                         continue;
                     }
 
@@ -109,24 +142,45 @@ for (int i = 0; i < inputToArray.Length; i++)
                         currentWord += $"{fifthLetter}";
                         if (currentWord == "green")
                         {
-                            Console.WriteLine(currentChar);
-                            Console.WriteLine(currentWord);
-                            //greenInHand += int.Parse($"{currentChar}");
+                            greenInHand += currentDigit;
                             continue;
                         }
                     }
 
 
                 }
-                
-               
+
+
             }
 
-            
+
 
         }
 
+        
+        Console.WriteLine("LINE");
+
     }
+    
+
+    //if (redInHand <= redPossibleGame && blueInHand <= bluePossibleGame && greenInHand <= greenPossibleGame)
+    //{
+
+    //    string[] newArr = current.Split("Game ");
+    //    string s = string.Join(" ", newArr);
+    //    string[] s2 = s.Split(":");
+    //    string s3 = string.Join(" ", s2);
+    //    string[] s4 = s3.Split(" ");
+    //    string s5 = string.Join("", s4);
+    //    string possibleNumberOfGame = s4[1];
+
+    //    Console.WriteLine(possibleNumberOfGame);
+    //    Console.WriteLine(current);
+    //    idOfThePossibleGames += int.Parse(possibleNumberOfGame);
+
+    //}
 
 
 }
+
+Console.WriteLine(idOfThePossibleGames);
