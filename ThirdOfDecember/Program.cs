1589bc7bbc2040af1ec3ca4775a78c7898a31951
Martin <88441320+MartoIT@users.noreply.github.com>
2023-12-23 15:34:40 +0200

    start working on a task from december third..it will be crazy..fun...crazy

---
+++ ThirdOfDecember/Program.cs
@@ -0,0 +1,28 @@
+﻿string input = File.ReadAllText("D:\\advent-of-code-2023\\ThirdOfDecember\\input.txt");
+//string input = File.ReadAllText("C:\\Мартин\\Moi\\advent of code\\SecondOfDecember\\SecondOfDecember\\input.txt");
+
+string[] array = input.Split('\n');
+int firstLine = 0;
+int lastLine = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    string currentRow = array[i];
+    if (i == 0)
+    {
+        firstLine = 1;
+    }
+
+    if(i == array.Length - 1)
+    {
+        lastLine = 1;
+    }
+
+    for (int j = 0; j < currentRow.Length; j++)
+    {
+        Console.WriteLine(currentRow[j]);
+
+    }
+
+    Console.WriteLine("ROW");
+}
+
