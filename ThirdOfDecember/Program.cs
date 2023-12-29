374e29bc9a2ece0f60059f2f8edcf7104f7e3df5
MartoIT <88441320+MartoIT@users.noreply.github.com>
2023-12-29 11:43:46 +0200

    i adapted logic on a program and now is give the correct answer on a example engine schematic

--- ThirdOfDecember/Program.cs
+++ ThirdOfDecember/Program.cs
@@ -4,9 +4,8 @@ string input = File.ReadAllText("C:\\Мартин\\Moi\\advent of code\\SecondOf
 string[] array = input.Split('\n');
 int firstLine = 0;
 int lastLine = 0;
-
-
-Console.WriteLine(input);
+bool enginPart = false;
+int digitPartFromEngine = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
@@ -14,6 +13,10 @@ for (int i = 0; i < array.Length; i++)
     if (i == 0)
     {
         firstLine = 1;
+    }
+    else
+    {
+        firstLine = 0;
     }
 
     if (i == array.Length - 1)
@@ -23,16 +26,29 @@ for (int i = 0; i < array.Length; i++)
 
     for (int j = 0; j < currentRow.Length; j++)
     {
-        int currentDigit;
+        enginPart = false;
+        int currentDigit = 0;
         char currentSymbol = currentRow[j];
         char previousSymbol = '\0';
-        char endSymbol = '\0';
+        char afterSymbol = '\0';
+
+        int startIndex = -1;
+        int endIndex = -1;
 
 
         if (Char.IsDigit(currentSymbol))
         {
             currentDigit = int.Parse($"{currentSymbol}");
+            if (j > 0)
+            {
+                previousSymbol = currentRow[j - 1];
+                startIndex = j - 1;
+                if (previousSymbol != '.' && !Char.IsDigit(previousSymbol))
+                {
 
+                    enginPart = true;
+                }
+            }
         }
 
         if (j + 1 < currentRow.Length)
@@ -43,7 +59,19 @@ for (int i = 0; i < array.Length; i++)
             {
                 currentDigit = int.Parse($"{currentSymbol}{secondSymbol}");
                 j++;
+                if(startIndex == -1)
+                {
+                    startIndex = j;
+                }
 
+                if (j + 1 < currentRow.Length)
+                {
+                    endIndex = j + 1;
+                }
+                else
+                {
+                    endIndex = j;
+                }
                 if (j + 1 < currentRow.Length)
                 {
                     char thirdSymbol = currentRow[j + 1];
@@ -52,6 +80,15 @@ for (int i = 0; i < array.Length; i++)
                     {
                         currentDigit = int.Parse($"{currentSymbol}{secondSymbol}{thirdSymbol}");
                         j++;
+                        if(j + 1 < currentRow.Length)
+                        {
+                            endIndex = j + 1;
+                        }
+                        else
+                        {
+                            endIndex = j;
+                        }
+                        
                     }
 
 
@@ -61,11 +98,76 @@ for (int i = 0; i < array.Length; i++)
 
         }
 
+        if (j + 1 < currentRow.Length)
+        {
+            afterSymbol = currentRow[j + 1];
+            if (afterSymbol != '.' && !Char.IsDigit(afterSymbol) && afterSymbol != '\0')
+            {
+                
+                enginPart = true;
+                j++;
+            }
+        }
 
+        if (enginPart)
+        {
+            digitPartFromEngine += currentDigit;
+            continue;
+        }
+
+        if (firstLine == 0 && currentDigit != 0)
+        {
+            string rowBefor = array[i - 1];
+            for (int k = startIndex; k <= endIndex; k++)
+            {
+                char symbolInRowAbove = rowBefor[k];
+                if(symbolInRowAbove != '.' && !Char.IsDigit(symbolInRowAbove))
+                {
+                    Console.WriteLine(currentDigit);
+                    enginPart = true;
+                    
+                }
+              
+
+                
+            }
+        }
+
+        if (enginPart)
+        {
+            digitPartFromEngine += currentDigit;
+            continue;
+        }
+
+        if (lastLine == 0 && currentDigit != 0)
+        {
+            string rowAfter = array[i + 1];
+            for (int l = startIndex; l <= endIndex; l++)
+            {
+                char symbolInRowUnder = rowAfter[l];
+                if (symbolInRowUnder != '.' && !Char.IsDigit(symbolInRowUnder))
+                {
+                    Console.WriteLine(currentDigit);
+                    enginPart = true;
+
+                }
+
+
+
+            }
+        }
+
+        if (enginPart)
+        {
+            digitPartFromEngine += currentDigit;
+            continue;
+        }
 
 
     }
 
-    Console.WriteLine("ROW");
+   
 }
 
+
+Console.WriteLine(digitPartFromEngine);
