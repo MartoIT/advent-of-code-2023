7c15c2e9b8a8a478c0bd68a7bc4b5d9a9d5e95e6
MartoIT <88441320+MartoIT@users.noreply.github.com>
2024-04-26 16:12:18 +0300

    Having fun with the Advent of code 2023 day 4, part 1 is ready, the answer was 20107; Continue with the next part of the assignment

---
+++ 04.fourthDay/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _04.fourthDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string input = File.ReadAllText("D:\\advent-of-code-2023\\ThirdOfDecember\\input.txt");
+            string[] input = File.ReadAllText("C:\\Мартин\\Moi\\advent of code\\SecondOfDecember\\04.fourthDay\\input.txt").Split("\n");
+
+            int points = 0;
+            int total = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] tokens = input[i].Split(":");
+                string[] bothColons = tokens[1].Split("|");
+
+                string leftSide = bothColons[0].Trim();
+                List<string> winningNumbers = leftSide.Split(" ").ToList();
+                winningNumbers.RemoveAll(s => string.IsNullOrWhiteSpace(s));
+
+                string rightSide = bothColons[1].Trim();
+                List<string> numbersElfHave = rightSide.Split(" ").ToList();
+                numbersElfHave.RemoveAll(s => string.IsNullOrWhiteSpace(s));
+
+
+                for (int j = 0; j < winningNumbers.Count; j++)
+                {
+                    int currentWiningNumber = int.Parse(winningNumbers[j]);
+
+                    for (int k = 0; k < numbersElfHave.Count; k++)
+                    {
+                        int currentElfNumber = int.Parse(numbersElfHave[k]);
+                        if(currentWiningNumber == currentElfNumber)
+                        {
+                            if(points == 0)
+                            {
+                                points = 1;
+                            }
+                            else
+                            {
+                                points = points * 2;
+                            }
+                        }
+                    }
+                }
+
+                total += points;
+                points = 0;
+
+            }
+
+            Console.WriteLine(total);
+
+        }
+    }
+}
