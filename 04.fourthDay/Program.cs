5b14595fe36233813b69b934c28fd9b0dec08c18
MartoIT <88441320+MartoIT@users.noreply.github.com>
2024-04-26 16:53:42 +0300

    Start working on a part 2 on day 4

--- 04.fourthDay/Program.cs
+++ 04.fourthDay/Program.cs
@@ -10,6 +10,8 @@
 
             int points = 0;
             int total = 0;
+            int coppyOfWinningCards = 0;
+            int original = 0;
             for (int i = 0; i < input.Length; i++)
             {
                 string[] tokens = input[i].Split(":");
@@ -23,16 +25,19 @@
                 List<string> numbersElfHave = rightSide.Split(" ").ToList();
                 numbersElfHave.RemoveAll(s => string.IsNullOrWhiteSpace(s));
 
-
+                int some = 0;
                 for (int j = 0; j < winningNumbers.Count; j++)
                 {
+                   
                     int currentWiningNumber = int.Parse(winningNumbers[j]);
-
+                    original++;
                     for (int k = 0; k < numbersElfHave.Count; k++)
                     {
                         int currentElfNumber = int.Parse(numbersElfHave[k]);
                         if(currentWiningNumber == currentElfNumber)
                         {
+                           
+                            coppyOfWinningCards++;
                             if(points == 0)
                             {
                                 points = 1;
@@ -44,13 +49,13 @@
                         }
                     }
                 }
-
+               
                 total += points;
                 points = 0;
 
             }
-
-            Console.WriteLine(total);
+            
+            Console.WriteLine(original);
 
         }
     }
